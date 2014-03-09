c3750fa8bbbbed4642292b38577527e8d68a16a4
Vivian Leung <vivianleung19@gmail.com>
2014-03-09 10:45:29 +0100

    diffGraph - made scale option, consensus v all data views

--- Problem2/differenceGraph.js
+++ Problem2/differenceGraph.js
@@ -11,22 +11,46 @@ bbVis = {
   x: 0 + 100, y: 10, w: width - 100, h: 300
 };
 
+var padding = 10;
+
 dataSet = [];
+var datum;
+var getSet = function(attr) {
+  var set = [];
+  dataSet.forEach(function(d) {
+    d.values.forEach(function(e){ set[e[attr]] = e[attr]; }) 
+  });
+  return set;
+}  
 
 d3.csv("timeline.csv", function(error, data) {
   if (error) {console.log(error); }
   else {
     var keys = d3.keys(data[0]);
     keys.splice(keys.indexOf('Year'), 1);
-
     dataSet = keys.map( function(source) {
       var values = [];
       data.forEach( function(d) { if (d[source] && d.Year >= 0) { 
-        values.push({'year': parseInt(d.Year), 'population': parseInt(d[source]) }); }
+        d.Year = parseInt(d.Year);
+        d[source] = parseInt(d[source]);
+        values.push({'year': d.Year, 'population': d[source] }); 
+        }
       });
       values.sort(function(a, b){ return a.year - b.year; });
       return {'source': source, 'values': values};
     });
+
+    var consensus= [];
+    data.forEach(function(t) { if (t.Year >= 0) {
+        var sum = 0;
+        var num = 0;
+        keys.forEach(function(d) { if (t[d]) { sum = sum+t[d]; num++; } })
+        if (num > 0) {
+          var avg = Math.floor(sum/num);
+          consensus.push({'year': t.Year, 'population': avg });}
+      }
+    })
+    dataSet.push({source: 'consensus', 'values': consensus});
 
     return createVis();
   }
@@ -37,20 +61,12 @@ createVis = function() {
 
   var xDom, xAxis, xScale, yAxis,  yScale, srcScale;
 
-  var getSet = function(attr) {
-    var set = [];
-    dataSet.forEach(function(d) {
-      d.values.forEach(function(e){ set[e[attr]] = e[attr]; }) 
-    });
-    return set;
-  }  
-  
+    
   // setting scales and axes
-  xDom = getSet('year');
-  xScale = d3.scale.linear().domain(d3.extent(d3.values(xDom)))
-            .range([0, bbVis.w]);
-  yScale = d3.scale.linear().domain(d3.extent(d3.values(getSet('population'))))
-            .range([bbVis.h, 0]);
+  xDom = d3.extent(d3.values(getSet('year')));
+  yDom = d3.extent(d3.values(getSet('population')));
+  xScale = d3.scale.linear().domain(xDom).range([0, bbVis.w]);
+  yScale = d3.scale.linear().domain(yDom).range([bbVis.h, 0]);
   srcScale = d3.scale.category10().domain(dataSet.map(function(d) { 
             return d.source;} ));
   xAxis = d3.svg.axis().scale(xScale).orient("bottom");
@@ -65,12 +81,8 @@ createVis = function() {
   // setting constructors
   var line = d3.svg.line()
   .interpolate("linear")
-  .x(function(e) { 
-    return xScale(e.year) + bbVis.x; })
-  .y(function(e) { 
-    return yScale(e.population); });
-
-
+  .x(function(e) { return xScale(e.year) + bbVis.x; })
+  .y(function(e) { return yScale(e.population); });
 
   svg = d3.select("#vis").append("svg").attr({
     width: width + margin.left + margin.right,
@@ -79,20 +91,13 @@ createVis = function() {
       transform: "translate(" + margin.left + "," + margin.top + ")"
     });
 
-  svg.append("clipPath").attr("id", "clip")
-    .append("rect")
-    .attr({x: bbVis.x, y: bbVis.y, 
-      width: bbVis.w, 
-      height: bbVis.h});
 
   // append svgX, Y axes
   svg.append("g").attr("class", "x axis")
     .attr("transform", "translate("+bbVis.x+","+(bbVis.y + bbVis.h)+")")
     .call(xAxis)
     .append("text")
-    .attr("x", bbVis.w)
-    .attr("dx", 0)
-    .attr("dy",  45)
+    .attr({x: bbVis.w, dy: 45, class: "label"})
     .style("text-anchor", "end")
     .text("Year");
 
@@ -100,12 +105,15 @@ createVis = function() {
     .attr("transform", "translate("+bbVis.x+",0)")
     .call(yAxis)
     .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", bbVis.y)
-    .attr("dy", 10)
-    .attr("dx", -2)
+    .attr({transform:"rotate(-90)", y: bbVis.y, dy:10, dx:-2, class:"label"})
     .style("text-anchor", "end")
     .text("Population");
+
+  svg.append("clipPath").attr("id", "clip")
+    .append("rect")
+    .attr({ width: bbVis.w+padding, height: bbVis.h+bbVis.y+padding,
+      transform: "translate("+bbVis.x+","+(padding*-1)+")"
+    });
   
   svg.append("rect")
     .attr("class", "overlay")
@@ -113,21 +121,56 @@ createVis = function() {
     .attr("height", height)
     .call(zoom);
 
-  var sources = svg.selectAll(".source").data(dataSet)
-      .enter().append("g").attr("class", "source");
+  var sources, lines, points;
+  var makeDataViz = function() {
+    sources = svg.selectAll(".source").data(dataSet);
 
-  var lines = sources.append("path").attr("class", "line")
-    .attr("d", function(d) {return line(d.values) })
-    .attr("clip-path", "url(#clip)")
-    .style("stroke", function(d) {return srcScale(d.source)});
+    sources.enter().append("g").attr("class", function(d) {return "source "+d.source});
 
-  var points = sources.append("g").attr("class", "points")
-      .attr("fill", function(d) {return srcScale(d.source)})
+    lines = sources.append("path").attr("class", function(d) {return "line "+d.source})
+      .attr("d", function(d) {return line(d.values) })
       .attr("clip-path", "url(#clip)")
-      .selectAll(".point")
-      .data(function(d) {return d.values})
-      .enter().append("svg:circle")
-      .call(transform);
+      .style("stroke", function(d) {return srcScale(d.source)});
+
+    points = sources.append("g").attr("class", function(d) {return "points "+d.source})
+        .attr("fill", function(d) {return srcScale(d.source)})
+        .attr("clip-path", "url(#clip)")
+        .selectAll(".point")
+        .data(function(d) {return d.values})
+        .enter().append("svg:circle")
+        .call(transform);
+
+  }
+
+  makeDataViz();
+
+  // change from / reset linear and log scales
+  d3.selectAll("input[name=scale]").on("click", function() {
+    if ($(this).attr("value") == "linear") {yScale = d3.scale.linear().range([bbVis.h, 0]); }
+    else  {yScale = d3.scale.log().range([bbVis.h, 0]); }
+      xScale.domain(xDom);
+      yScale.domain(yDom);
+      xAxis.scale(xScale);
+      yAxis.scale(yScale);
+      zoom.x(xScale).y(yScale);
+      zoomed();
+  })
+
+  // switch views from all data to consensus analysis
+  d3.selectAll("input[name=consensus]").on("click", function() {
+    console.log($(this).attr("value"))
+    if ($(this).attr("value") == "trends"){
+      var sGs = d3.selectAll('.source:not(.consensus)')
+      sGs.data([]);
+      sGs.remove();
+    } 
+    else {
+      var remaining = d3.selectAll('.source')
+      remaining.data([]);
+      remaining.remove();
+      return makeDataViz();
+    }
+  })
 
   function zoomed(){
     svg.select(".x.axis").call(xAxis);
@@ -140,7 +183,7 @@ createVis = function() {
   function transform(s){
     s.attr("cx", function(e) {return xScale(e.year) + bbVis.x;})
         .attr("cy", function(e) {return yScale(e.population); })
-        .attr("r", 5);
+        .attr("r", 3);
 
   }
 
