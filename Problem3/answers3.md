390af52ab48227648e6ab95843b9ce29c97a8ba1
Vivian Leung <vivianleung19@gmail.com>
2014-03-09 01:34:21 +0000

    finished brush

---
+++ Problem3/answers3.md
@@ -0,0 +1,7 @@
+<h1>Problem 3 Answers</h1>
+
+<ol>
+	<li>HTML element of interactive brush area is an svg "rect" element with class "background", within a "g" group classed as "brush".</li>
+	<li>HTML element that represents the selection is an svg "rect" element classed as "extent", within the brush group. </li>
+	<li>The other DOM elements, namely two rects contained inside two g classed "resize w" and "resize e", bound the selection and mark the min and max of the selection, respectively.</li>
+</ol>
--- Problem3/unHealth.js
+++ Problem3/unHealth.js
@@ -44,6 +44,8 @@ d3.csv("unHealth.csv", function(error, data) {
 
     var parseDate = d3.time.format("%B %Y").parse;
 
+    // for applicability's sake (if one wanted to add more datasets)
+    // each category (e.g. "women's health") is grouped by 'cat'
     dataSet = keys.map( function(cat) {
       var values = [];
       data.forEach( function(d) { 
@@ -54,112 +56,139 @@ d3.csv("unHealth.csv", function(error, data) {
     });
     return createOverview();
 }});
-var scales = [];
 
 createOverview = function() {
-  var xDom, xAxis, xScale, yAxis,  yScale;
+  var over, detail, scalesOverview, scalesDetail, axesOverview, axesDetail, area;
+  var catOver, catDetail, pointsOver, pointsDetail, linesOver, linesDetail, brush;
+  var brushPadding = 5;
 
-  var getSet = function(attr) {
+  var axisDate = d3.time.format('%b %y');
+
+  over = svg.append("g").attr({ class: "overview",
+    transform: "translate(" + margin.left + "," + margin.top + ")"
+  })
+
+  detail = svg.append("g").attr({ class: "detail", 
+    transform: "translate(" + margin.left + "," + (margin.top+bbDetail.y+padding) + ")"
+  });
+
+  detail.append("clipPath").attr("id", "clip").append("rect")
+    .attr({x:bbDetail.x, y:bbDetail.y, width:bbDetail.w, height:bbDetail.h});
+
+  scalesOverview = makeScales(bbOverview);
+  scalesDetail = makeScales(bbDetail);
+  axesOverview = makeAxes(bbOverview, scalesOverview, over);
+  axesDetail = makeAxes(bbDetail, scalesDetail, detail);
+
+  brush = d3.svg.brush().x(scalesOverview.x).on("brush", brushed);
+
+  catOver = over.selectAll(".cat").data(dataSet)
+      .enter().append("g").attr("class", "cat");
+
+  catDetail = detail.selectAll(".cat").data(dataSet)
+    .enter().append("g").attr("class", "cat");
+
+  var lineOver = d3.svg.line().interpolate("linear")
+      .x(function(d) { return scalesOverview.x(d.date) + bbOverview.x; })
+      .y(function(d) { return scalesOverview.y(d.value)  + bbOverview.y; });
+  
+  var lineDetail = d3.svg.line().interpolate("linear")
+      .x(function(d) { return scalesDetail.x(d.date) + bbDetail.x; })
+      .y(function(d) { return scalesDetail.y(d.value)  + bbDetail.y; });
+
+  pointsOver = makePoints(catOver, scalesOverview, bbOverview);
+  pointsDetail = makePoints(catDetail, scalesDetail, bbDetail)
+                .attr("clip-path", "url(#clip)");
+  linesOver = makeLines(catOver, lineOver);
+  linesDetail = makeLines(catDetail, lineDetail)
+                .attr("clip-path", "url(#clip)");
+
+  area = d3.svg.area().interpolate("linear")
+        .x(function(d) {return scalesDetail.x(d.date) + bbDetail.x; })
+        .y0(scalesDetail.y(0) + bbDetail.y - 3)
+        .y1(function(d) {return scalesDetail.y(d.value) + bbDetail.y ; });
+
+  detail.append("path").data(dataSet)
+  .attr({class: "detailArea"})
+  .attr("d", function(d) {return area(d.health); })
+  .attr("clip-path", "url(#clip)");
+
+  over.append("g").attr("class", "brush").call(brush)
+    .selectAll("rect").attr({
+      height: bbOverview.h + brushPadding,
+      y: bbOverview.y - brushPadding
+  });
+
+  // gets the set of values of a prop across nested arrays of objects
+  function getSet(attr) {
     var set = [];
     dataSet.forEach(function(d) {
       d.health.forEach(function(e){ set[e[attr]] = e[attr]; }) 
     });
     return d3.values(set);
   }  
-  var axisDate = d3.time.format('%b %y');
-  xScale = d3.time.scale().domain(d3.extent(getSet('date')));
-  yScale = d3.scale.linear().domain(d3.extent(getSet('value')));
 
-  xAxis = d3.svg.axis().scale(xScale).orient("bottom")
-            .tickFormat(function(d) {return axisDate(d)});
-  yAxis = d3.svg.axis().scale(yScale).orient("left")
-            .tickFormat( function(d) {return d/1000});
+  function makeScales (bb) {
+    return { x: d3.time.scale().domain(d3.extent(getSet('date'))).range([0, bb.w]),
+             y: d3.scale.linear().domain(d3.extent(getSet('value'))).range([bb.h, 0])};
+  }
 
-  var over = svg.append("g").attr({ class: "overview",
-    transform: "translate(" + margin.left + "," + margin.top + ")"
-  })
+  function makeAxes (bb, scales, g) {
+    var xAxis = d3.svg.axis().scale(scales.x).orient("bottom")
+                .tickFormat(function(d) {return axisDate(d)});
 
-  var detail = svg.append("g").attr({ class: "detail", 
-    transform: "translate(" + margin.left + "," + (margin.top+bbDetail.y+padding) + ")"
-  });
-
-  [[over, bbOverview], [detail, bbDetail]].forEach(function(g) {
-
-    xScale.range([0, g[1].w]);
-    yScale.range([g[1].h,0]);
-    yAxis.ticks(g[1].h / 15);
-
-
-
-    g[0].append("g").attr("class", "x axis")
-      .attr("transform", "translate("+g[1].x+","+g[1].h+")")
+    var yAxis = d3.svg.axis().scale(scales.y).orient("left")
+                .tickFormat( function(d) {return d/1000})
+                .ticks(bb.h / 15); 
+  
+    g.append("g").attr("class", "x axis")
+      .attr("transform", "translate("+bb.x+","+(bb.h+bb.y)+")")
       .call(xAxis)
       .selectAll("text")
       .attr({'text-anchor': 'start', 'transform': "rotate(-45)","dy": 10, "dx": -30})
     
-    g[0].select('g.x.axis').append("text")
-      .attr({x: g[1].w, dy: 75, class: 'label' })
+    g.select('g.x.axis').append("text")
+      .attr({x: bb.w, dy: 75, class: 'label' })
       .style("text-anchor", "end")
       .text("Analysis Date");
 
-    g[0].append("g").attr("class", "y axis")
-    .attr("transform", "translate("+g[1].x+",0)")
+    g.append("g").attr("class", "y axis")
+    .attr("transform", "translate("+bb.x+","+bb.y+")")
     .call(yAxis)
+    .append("text")
+    .attr({transform: "rotate(-90)", dy: -45, dx: -2, class: 'label'})
+    .style("text-anchor", "end")
+    .text("pulse, 1000s");
 
-    g[0].select('g.y.axis').append("text")
-      .attr({transform: "rotate(-90)", dy: -45, dx: -2, class: 'label'})
-      .style("text-anchor", "end")
-      .text("pulse, 1000s");
 
-    var tickSpace = g[1].x+d3.select('g.y.axis')[0][0].getBBox().width-g[1].w/xScale.ticks().length;
-
-    var cat = g[0].selectAll(".cat").data(dataSet)
-        .enter().append("g").attr("class", "cat")
-
-    var points = cat.append("g").attr("class", "points").selectAll(".point")
-        .data(function(d) { return d.health})
-        .enter().append("svg:circle")
-        .attr("cx", function(e) { return xScale(e.date) + g[1].x;})
-        .attr("cy", function(e) { return yScale(e.value); })
+    return { x: xAxis, y: yAxis} ;
+  }
+  
+  function makePoints(cat, scales, bb) {
+    return cat.append("g").attr("class", "points").selectAll(".point")
+      .data(function(d) { return d.health})
+      .enter().append("svg:circle")
+        .attr("cx", function(e) { return scales.x(e.date) + bb.x;})
+        .attr("cy", function(e) { return scales.y(e.value) + bb.y; })
         .attr("r", 3);
+  }
 
-
-
-    var line = d3.svg.line().interpolate("linear")
-        .x(function(d) { return xScale(d.date) + g[1].x; })
-        .y(function(d) { return yScale(d.value); });
-
-    var lines = g[0].selectAll('.cat').append("path")
-      .attr("class", "path "+g[0].attr("class"))
-      .attr("d", function(d) {return line(d.health) });
-    
-    if (g[0] == detail) {
-
-      var area = d3.svg.area().interpolate("linear")
-            .x(function(d) {return xScale(d.date) + bbDetail.x; })
-            .y0(yScale(0) - 3)
-            .y1(function(d) {return yScale(d.value) ; });
-
-      detail.append("path").data(dataSet)
-      .attr({class: "detailArea"})
-      .attr("d", function(d) {return area(d.health); });
-
-    }
-    
-  });
+  function makeLines(g, line) {
+    return g.append("path")
+    .attr("class", "path "+g.attr("class"))
+    .attr("d", function(d) {return line(d.health) });
+  }
+  
+  function brushed() {
+    // if brush selection is empty, use original domain, else use brush extent
+    scalesDetail.x.domain(brush.empty() ? scalesOverview.x.domain() : brush.extent());
+    detail.select(".detailArea").attr("d", function(d) {return area(d.health);});
+    detail.select(".path.cat").attr("d", function(d) {return lineDetail(d.health);});
+    detail.selectAll("circle")
+        .attr("cx", function(e) { return scalesDetail.x(e.date) + bbDetail.x;})
+        .attr("cy", function(e) { return scalesDetail.y(e.value) + bbDetail.y; });
+    detail.select(".x.axis").call(axesDetail.x);
+  }
 };
-
-
-/*    var rects = cat.append("g").attr("class", "rects").selectAll(".rect")
-            .data(function(d) { return d.health})
-            .enter().append("svg:rect")
-            .attr("x", function(e) { return xScale(e.date)+tickSpace;})
-            .attr("y", g[1].y - 2*xAxis.tickSize())
-            .attr("width", xAxis.tickSize()*2)
-            .attr("height", g[1].h)
-            .attr("fill", 'none')
-            .on("mouseover", function() { console.log(d3.select(this)); return d3.select(this).attr("fill", "red")})
-            .on("mouseout", function() { return d3.select(this).attr("fill", "none")});
-*/
 
 
