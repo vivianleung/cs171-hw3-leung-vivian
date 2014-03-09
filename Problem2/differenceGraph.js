61316043ecaf321333c348c36cb8a4955a551089
Vivian Leung <vivianleung19@gmail.com>
2014-03-09 23:57:46 +0100

    problems with re-enter. will try to separate sources

--- Problem2/differenceGraph.js
+++ Problem2/differenceGraph.js
@@ -1,5 +1,5 @@
 var bbVis, brush, createVis, dataSet, handle, height, margin, svg, svg2, width, zoom;
-
+var sources;
 margin = {
   top: 50, right: 50, bottom: 50, left: 50
 };
@@ -12,6 +12,12 @@ bbVis = {
 };
 
 var padding = 10;
+var LBoxLen = 10;
+
+var Yinput = 'population';
+var lastConsensusInput = 'all';
+var lastDivergenceSelected = 'raw';
+var lastDivergeTypeInput = 'absolute';
 
 dataSet = [];
 
@@ -50,8 +56,7 @@ d3.csv("timeline.csv", function(error, data) {
         d.Year = parseInt(d.Year);
         d[source] = parseInt(d[source]);
         var absDiff = d[source] - consensus[d.Year].population;
-        var percDiff = absDiff/consensus[d.Year].population * 100;
-
+        var percDiff = (absDiff/consensus[d.Year].population) * 100;
         values.push({'year': d.Year, 'population': d[source], 'absolute': absDiff, 'percent': percDiff }); 
         }
       });
@@ -59,7 +64,6 @@ d3.csv("timeline.csv", function(error, data) {
       return {'source': source, 'values': values};
     });
     dataSet.push({source: 'consensus', 'values': d3.values(consensus)});
-
 
     return createVis();
   }
@@ -69,8 +73,7 @@ d3.csv("timeline.csv", function(error, data) {
 
 createVis = function() {
 
-  var xDom, yDom, xAxis, xScale, yScale, yAxis, srcScale, line, consensusArea, consensusAreaMin;
-
+  var xDom, yDom, xAxis, xScale, yScale, yAxis, srcScale, line, consensusArea, consensusAreaMin, sSelected;
     
   // setting scales and axes
   xDom = d3.extent(d3.values(getSet('year')));
@@ -88,15 +91,20 @@ createVis = function() {
   .x(function(e) { return xScale(e.year) + bbVis.x; })
   .y(function(e) { return yScale(e.population); });
 
+  area = d3.svg.area().interpolate("linear")
+    .x(function(d) {return xScale(d.year) + bbVis.x })
+    .y0(yScale(0))
+    .y1(function(d) { return yScale(d[Yinput]) });
+
+  zoom = d3.behavior.zoom().x(xScale).y(yScale).scaleExtent([1, 50]).on("zoom", zoomed);
 
   svg = d3.select("#vis").append("svg").attr({
     width: width+margin.left+margin.right, height:height+margin.top+margin.bottom})
-    .append("g").attr({
+    .append("g").attr({ class: 'graph',
       transform: "translate(" + margin.left + "," + margin.top + ")"
     });
 
-
-  // append svgX, Y axes
+  // append svgX, Y axes, clip path
   svg.append("g").attr("class", "x axis")
     .attr("transform", "translate("+bbVis.x+","+(bbVis.y + bbVis.h)+")")
     .call(xAxis)
@@ -119,18 +127,47 @@ createVis = function() {
       transform: "translate("+bbVis.x+","+(padding*-1)+")"
     });
 
-  zoom = d3.behavior.zoom().x(xScale).y(yScale).scaleExtent([1, 50]).on("zoom", zoomed);
+  // append legend
+  var legendBox = d3.select('svg').append("g").attr("class", 'legendBox')
+    .attr("transform", function(d) {
+      var ly = margin.top+bbVis.h+d3.select('g.x.axis')[0][0].getBBox().height+padding;
+      sSelected = dataSet.length;
+      return "translate("+margin.left+","+ly+")"});
+  
+  var legend = legendBox.append("g").attr("class", "legend")
+    .selectAll("g").data(dataSet.map(function(d, i){return [d.source, i]}))
+    .enter().append("g")
+    .attr("class", function(d) { return d[0]+" selected"})
+    .attr("id", function(d) {return "k"+d[1]})
+    .attr("fill", function(d) {return srcScale(d[0])})
+    .on("click", selectThis);
+  
+  legend.append("rect")
+    .attr({ width: LBoxLen, height: LBoxLen })
+    .attr("y", function(d, i) {return i * 18 - LBoxLen});
 
-  var sources, lines, points, areaMin, area, divergeAreas;
+  legend.append("text")
+    .attr("x", 20)
+    .attr("vertical-align", "text-top")
+    .attr("y", function(d, i) {return i * 18})
+    .text(function(d) {return d[0]});
 
-  var Yinput = 'population',
-      lastDivergeTypeInput = 'absolute';
+  legendBox.append("g").selectAll('text')
+    .data(['DESELECT ALL', 'SELECT ALL'])
+    .enter().append('text')
+    .attr("class", "legselectors")
+    .attr("transform", function(d, i)  {
+      var gy = d3.select('.legend')[0][0].getBBox().height+padding+i*18;
+     return "translate(0,"+gy+")";})
+    .on("click", selectThis)
+    .text(function(d) {return d});
+
+  var lines, points, areaMin, area, divergeAreas;
+  var removedSources = {};
 
   var makeDataViz = function() {
 
-
     sources = svg.selectAll(".source").data(dataSet);
-
     sources.enter().append("g").attr("class", function(d) {return "source "+d.source});
 
     lines = sources.append("path").attr("class", function(d) {return "line "+d.source})
@@ -145,16 +182,6 @@ createVis = function() {
         .enter().append("svg:circle")
         .call(transform);
 
-    area = d3.svg.area().interpolate("linear")
-      .x(function(d) {return xScale(d.year) + bbVis.x })
-      .y0(yScale(bbVis.h))
-      .y1(function(d) { return yScale(d.max) });
-
-    areaMin = d3.svg.area().interpolate("linear")
-      .x(function(d) {return xScale(d.year) + bbVis.x})
-      .y0(yScale(bbVis.h))
-      .y1(function(d) { return yScale(d.min) });
-
     svg.append("rect")
       .attr({class: "overlay", width: width, height: height})
       .call(zoom);
@@ -167,104 +194,188 @@ createVis = function() {
     .on("keyup", function() { zoom.y(yScale);  })
     .on("keydown", function() { if(d3.event.altKey) {zoom.y(null); } });
 
+  var lastScaleInput = 'linear';
   // change from / reset linear and log scales
   d3.selectAll("input[name=scale]").on("click", function() {
-    if ($(this).attr("value") == "linear") {yScale = d3.scale.linear().range([bbVis.h, 0]); }
-    else  {yScale = d3.scale.log().range([bbVis.h, 0]); }
+    lastScaleInput = $(this).attr("value");
+    scaleChange();
+    resetParams();
+    zoom();
+  });
+
+  function scaleChange () {
+    if (lastScaleInput == "linear") {
+      yScale = d3.scale.linear().range([bbVis.h, 0]);
+    }
+    else  {
+      yScale = d3.scale.log().range([bbVis.h, 0]); 
+    }
       xScale.domain(xDom);
       yScale.domain(yDom);
-      resetParams();
-  });
+
+  }
+
+  
 
   // switch views from all data to consensus analysis
   d3.selectAll("input[name=consensus]").on("click", function() {
-    if ($(this).attr("value") == "trends"){
-      var sGs = d3.selectAll('.source:not(.consensus)')
-      sGs.data([]);
-      sGs.remove();
-      svg.select('rect.overlay').remove();
+    if ($(this).attr("value") != lastConsensusInput){
+      lastConsensusInput = $(this).attr("value");
+      if (lastConsensusInput == 'trends') {
 
-      var consensus = d3.select('.source.consensus');
-      consensusArea = consensus.append("path")
-              .attr("d", function(c) {return area(c.values)})
-              .attr({fill: 'steelblue', 'fill-opacity': 0.5, class: '.source.consensus'})
-              .attr("clip-path", "url(#clip)");
+        var sGs = d3.selectAll('.source:not(.consensus)')
+        sGs.data([]);
+        sGs.remove();
+        svg.select('rect.overlay').remove();
 
-      consensusAreaMin = consensus.append("path")
-              .attr("d", function(c) {return areaMin(c.values)})
-              .attr({fill: 'white', 'fill-opacity': 1, class: '.source.consensus'})
-              .attr("clip-path", "url(#clip)");
+        legend.classed('selected', false);
+        legend.select('.consensus').classed('selected', true);
 
-    } 
-    else {
-      var remaining = d3.selectAll('.source')
-      remaining.data([]);
-      remaining.remove();
-      svg.select('rect.overlay').remove();
-      console.log("elsed");
-      return makeDataViz();
+
+        makeTrends();
+        resetParams();
+        zoomed();
+
+      } 
+      else {
+        var remaining = d3.selectAll('.source')
+        remaining.data([]);
+        remaining.remove();
+        svg.select('rect.overlay').remove();
+        Yinput="population";
+
+        makeDataViz();
+        zoomed();
+      }
     }
   });
+
+  function makeTrends() {
+    xScale.domain(xDom);
+    yScale.domain(yDom);
+
+    var consensus = d3.select('.source.consensus');
+    if (!consensus[0][0]) { 
+      var sourceData = dataMap[dataMap.length-1]
+      consensus = sources.append("g").data(sourceData)
+          .attr("class", function(d) {return "source "+d.source});
+    }
+    if (!consensus.select('.line')) {
+      consensus.append("path").attr("class", function(d) {return "line "+d.source})
+        .style("stroke", function(d) {return srcScale(d.source)})
+        .attr("clip-path", "url(#clip)");         
+    }
+    if (!consensus.select('.points')){
+      consensus.append("g").attr("class", function(d) {return "points "+d.source})
+        .attr("fill", function(d) {return srcScale(d.source)})
+        .attr("clip-path", "url(#clip)")
+        .selectAll(".point")
+        .data(function(d) {return d.values})
+        .enter().append("svg:circle");
+
+    }
+
+    Yinput = 'max';
+    consensusArea = consensus.append("path")
+            .attr({fill: 'steelblue', 'fill-opacity': 0.5, class: '.source.consensus'})
+            .attr("clip-path", "url(#clip)");
+
+    Yinput = 'min';
+    consensusAreaMin = consensus.append("path")
+            .attr({fill: 'white', 'fill-opacity': 1, class: '.source.consensus'})
+            .attr("clip-path", "url(#clip)");
+
+    svg.append("rect")
+      .attr({class: "overlay", width: width, height: height})
+      .call(zoom);
+    Yinput = "population";
+  }
 
   // switch to viz for divergence
   d3.selectAll("input[name=divergence]").on("click", function() { 
-    if ($(this).attr("value") == 'diverge') {
-      console.log(Yinput);
-      if (Yinput == 'population') {Yinput = 'absolute';}
-      lastDivergenceSelected = 'diverge';
 
-      if (consensusArea) { consensusArea.remove(); consensusAreaMin.remove()}
-      yDom = d3.extent(d3.values(getSet(Yinput)));
-      yScale.domain(yDom);
+    if ($(this).attr("value") != lastDivergenceSelected) {
+      lastDivergenceSelected = $(this).attr("value");
+      if (lastDivergenceSelected == 'diverge') {
+        makeDiverge();
+        resetParams();
+        zoomed();
+      }
 
-
-      divergeAreas = d3.select("svg g").selectAll('.source').append("path")
-          .attr("class", function(d) {console.log(d.source); return 'area '+d.source})
-          .attr("fill", function(d) {return srcScale(d.source)})
-          .attr("fill-opacity", 0.3)
-          .attr("clip-path", "url(#clip)");
-      svg.append("rect")
-        .attr({class: "overlay", width: width, height: height})
-        .call(zoom);
-      resetParams();
-    }
-
-    else {
-      d3.selectAll('.source').selectAll('.area')
+      else {
+        d3.selectAll('.source').selectAll('.area').remove();
+        Yinput = 'population';
+        yDom = d3.extent(d3.values(getSet(Yinput)));
+        lastDivergenceSelected = 'raw';
+        scaleChange();
+        resetParams();
+        makeDataViz();
+        zoomed();
+      }
     }
 
   });
 
-/*  d3.selectAll("input[name=divergeType]").on("click", function() { 
-    if (lastDivergeSelected == 'diverge') {
-      if ($(this).attr('value') == 'percent')  {
+  function makeDiverge() {
+    Yinput = lastDivergeTypeInput;
+    yDom = d3.extent(d3.values(getSet(Yinput)));
+    xScale.domain(xDom);
+    yScale.domain(yDom);
 
-      }
-      else {
+    d3.selectAll('.source').remove();
+    sources = svg.selectAll(".source").data(dataSet);
+    sources.enter().append("g").attr("class", function(d) {return "source "+d.source});
 
-      }
+    divergeAreas = sources.append("path")
+        .attr("class", function(d) {return 'area '+d.source})
+        .attr("fill", function(d) {return srcScale(d.source)})
+        .attr("fill-opacity", 0.3)
+        .attr("clip-path", "url(#clip)");
+
+    svg.append("rect")
+      .attr({class: "overlay", width: width, height: height})
+      .call(zoom);
+
+  }
+
+  d3.selectAll("input[name=divergeType]").on("click", function() { 
+    if (lastDivergenceSelected == 'diverge' && $(this).attr('value') != lastDivergeTypeInput)  {
+        Yinput = $(this).attr('value');
+        lastDivergeTypeInput = Yinput;
+        yDom = d3.extent(d3.values(getSet(Yinput)));
+        yScale.domain(yDom);
+        resetParams();
+        zoomed();
     }
 
-  });*/
+  });
 
   function resetParams(){
     zoom.x(xScale).y(yScale);
     xAxis.scale(xScale);
     yAxis.scale(yScale);
+
     zoomed();
   }
 
   function zoomed(){
+    area.x(function(d) {return xScale(d.year) + bbVis.x })
+    .y0(yScale(0))
+    .y1(function(d) { return yScale(d[Yinput]) });
 
     line.y(function(e) {return yScale(e[Yinput])})
-    area.y0(yScale(0)).y1(function(d) { return yScale(d[Yinput] )});
     svg.select(".x.axis").call(xAxis);
     svg.select(".y.axis").call(yAxis);
     if (lines) {lines.attr("d", function(d) {return line(d.values) });}
     if (points) {points.call(transform);}
-    if (consensusArea) {consensusArea.attr("d", function(c) {return area(c.values)});}
-    if (consensusAreaMin) {consensusAreaMin.attr("d", function(c) {return areaMin(c.values)});}
-    if (divergeAreas) {divergeAreas.attr('d', function(d) { return area(d.values)});
+    if (lastConsensusInput == "trends") { 
+      datum = [consensusArea, area];
+      tempInput = Yinput;
+      consensusArea.attr("d", function(c) {Yinput = "max"; return area(c.values)});
+      consensusAreaMin.attr("d", function(c) {Yinput = "min"; return area(c.values)});
+      Yinput = tempInput;
+    }
+    if (lastDivergenceSelected == "diverge") {divergeAreas.attr('d', function(d) { return area(d.values)});
       d3.select('.source.consensus').select('.points').remove();
     }
 
@@ -273,7 +384,91 @@ createVis = function() {
   function transform(s){
     s.attr("cx", function(e) {return xScale(e.year) + bbVis.x;})
         .attr("cy", function(e) {return yScale(e[Yinput]); })
-        .attr("r", 3);
+        .attr("r", 2);
 
   }
+
+
+// SEPARATE SOURCES.
+// d3.selectAll('.line')[0].forEach(function(e) {console.log(e)})
+  function selectThis(){
+    var selectedS = d3.select(this);
+    console.log(selectedS);
+    if (selectedS.classed("legselectors")) {
+      if (selectedS.text() == "DESELECT ALL") {
+        removedSources = d3.selectAll('.source').remove();
+        return legend.classed("selected", false);
+      }
+      else { 
+        d3.select('.overlay').remove();
+        if (Yinput == "population") {
+          if (lastConsensusInput == "trends") { makeTrends(); }
+          else {           
+            legend.classed("selected", true);
+            makeDataViz();
+          }
+        }
+        else {
+          legend.classed("selected", true);
+          makeDiverge();
+        }
+        zoomed();
+      }
+    }
+    else {
+      var sName = '.'+selectedS.text().replace(/ /g,'.');
+      if (selectedS.classed("selected")) { 
+        selectedS.classed("selected",false)
+        selected
+        removed d3.select('.source'+sName).remove();
+      }
+      else {
+        d3.select(this).classed("selected", true);
+        d3.select('.overlay').remove();
+        if (Yinput == "population" && lastConsensusInput == "trends") { makeTrends(); }
+        else {           
+          legend.classed("selected", true);
+          var sourceData;
+          dataSet.forEach(function(d) {
+            if (d.source == selectedS.text()) {sourceData = d; return;}
+          });
+
+          var sourceG = sources.select(sName)
+          /*var sourceG = d3.select('g.graph').append("g").data(sourceData)
+            .attr("class", function(d) {return "source "+d.source});
+*/
+          if (Yinput == "population")  {
+            sourceG.append("path").attr("class", function(d) {return "line "+d.source})
+              .style("stroke", function(d) {return srcScale(d.source)})
+              .attr("clip-path", "url(#clip)");         
+
+            sourceG.append("g").attr("class", function(d) {return "points "+d.source})
+              .attr("fill", function(d) {return srcScale(d.source)})
+              .attr("clip-path", "url(#clip)")
+              .selectAll(".point")
+              .data(function(d) {return d.values})
+              .enter().append("svg:circle");
+          }
+          
+          else {
+            Yinput = lastDivergeTypeInput;
+            yDom = d3.extent(d3.values(getSet(Yinput)));
+            xScale.domain(xDom);
+            yScale.domain(yDom);
+
+            sourceG.append("path")
+                .attr("class", function(d) {return 'area '+d.source})
+                .attr("fill", function(d) {return srcScale(d.source)})
+                .attr("fill-opacity", 0.3).attr("clip-path", "url(#clip)");
+          }
+
+          svg.append("rect")
+            .attr({class: "overlay", width: width, height: height}).call(zoom);          
+        }
+        // for all options, need to zoom to re-draw
+        zoomed();
+      }
+    }
+  };
+
 };
